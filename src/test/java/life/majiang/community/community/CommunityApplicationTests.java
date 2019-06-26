1c7a14400fa8a0f732ffdcb0cd807c398463999b
yb <ybvip@outlook.com>
2019-06-26 19:28:56 +0800

    init repository

---
+++ src/test/java/life/majiang/community/community/CommunityApplicationTests.java
@@ -0,0 +1,15 @@
+package life.majiang.community.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootApplication
+public class CommunityApplicationTests {
+    public static void main(String[] args){
+        SpringApplication.run(CommunityApplication.class,args);
+    }
+}
