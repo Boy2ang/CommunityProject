896d8e697cce9777464750dcea423d0e4892fc93
yb <ybvip@outlook.com>
2019-07-08 16:14:44 +0800

    database

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GitHupUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import life.majiang.community.provider.GitHupProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GitHupProvider gitHupProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     //spring 初始化时会根据注解里面的键去application.properties把值注入进来
     @Value("${githup.client.id}")
@@ -30,7 +38,7 @@ public class AuthorizeController {
      * @return
      */
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code")String code,@RequestParam(name = "state")String state){
+    public String callback(@RequestParam(name = "code")String code, @RequestParam(name = "state")String state, HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -38,8 +46,22 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = gitHupProvider.getAccessToken(accessTokenDTO);
-        GitHupUser user = gitHupProvider.getUser(accessToken);
-        System.out.println(user);
-        return "index";
+        GitHupUser githupUser = gitHupProvider.getUser(accessToken);
+        if (githupUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githupUser.getName());
+            user.setAccountId(String.valueOf(githupUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功
+            request.getSession().setAttribute("githupUser",githupUser);
+            return "redirect:/";
+        }else {
+            //登录失败
+            return "redirect:/";
+
+        }
     }
 }
